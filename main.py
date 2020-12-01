98caf5017c5ebe0074cf286ae8bb89e72697e6d2
codekot <boobookish@gmail.com>
2020-12-01 17:19:18 +0400

    Create first draft interface for app

--- main.py
+++ main.py
@@ -1,26 +1,56 @@
+"""
+A program that stores book information:
+Title, Author,
+Year, ISBN
+
+User can:
+
+View all records
+Search an entry
+Add entry
+Update entry
+Delete
+Close
+"""
 from tkinter import *
 
 window = Tk()
 
+title_label = Label(window, text="Title")
+title_label.grid(row=0, column=0)
+title_value = Entry(window, textvariable=StringVar())
+title_value.grid(row=0, column=1)
 
-def km_to_miles():
-    try:
-        miles = round(float(entry1_value.get()) * 1.6, 2)
-    except:
-        miles = "Not a number"
-    text1.insert(END, miles)
+author_label = Label(window, text="Author")
+author_label.grid(row=0, column=2)
+author_value = Entry(window, textvariable=StringVar())
+author_value.grid(row=0, column=3)
 
-entry0 = Label(window,text="Km to Miles")
-entry0.grid(row=0, column=0)
+year_label = Label(window, text="Year")
+year_label.grid(row=1, column=0)
+year_value = Entry(window, textvariable=StringVar())
+year_value.grid(row=1, column=1)
 
-button1 = Button(window, text="Execute", command=km_to_miles)
-button1.grid(row=1, column=1)
+isbn_label = Label(window, text="ISBN")
+isbn_label.grid(row=1, column=2)
+isbn_value = Entry(window, textvariable=StringVar())
+isbn_value.grid(row=1, column=3)
 
-entry1_value = StringVar()
-entry1 = Entry(window, textvariable=entry1_value)
-entry1.grid(row=1, column=0)
+# Buttons
+view_all_btn = Button(window, text="View all")
+view_all_btn.grid(row=2, column=3)
+search_entry_btn = Button(window, text="Search entry")
+search_entry_btn.grid(row=3, column=3)
+add_entry_btn = Button(window, text="Add Entry")
+add_entry_btn.grid(row=4, column=3)
+update_btn = Button(window, text="Update")
+update_btn.grid(row=5, column=3)
+delete_btn = Button(window, text="Delete")
+delete_btn.grid(row=6, column=3)
+close_btn = Button(window, text="Close")
+close_btn.grid(row=7, column=3)
 
-text1 = Text(window, height=1, width=15)
-text1.grid(row=2, column=0)
+listbox = Text(window, height=6, width=15)
+listbox.grid(row=2, column=1)
 
 window.mainloop()
